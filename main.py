13332c8e8520bfbeb4b9e030ae710cce2abd9f7f
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2022-09-27 10:47:28 +0200

    Path / Query Parameters & Request Body & String Validations

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from enum import Enum
+from pydantic import BaseModel
+from fastapi import FastAPI, Query
 
 from fastapi import FastAPI
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
 class ModelName(str, Enum):
@@ -21,3 +30,22 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+async def read_items(q: str | None = Query(default=None, max_length=50)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
