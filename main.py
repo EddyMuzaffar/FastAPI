fd3d06440ae3fd1c6ca8af312316cb298cf67881
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2022-09-28 14:53:52 +0200

    Use db.json and use product

--- main.py
+++ main.py
@@ -1,164 +1,55 @@
-from fastapi import FastAPI, Path, Query, Body, Cookie, Header, Depends, HTTPException, status
-from fastapi.testclient import TestClient
-from enum import Enum
-from pydantic import BaseModel, Field, HttpUrl, EmailStr
-from datetime import datetime, time, timedelta
-from uuid import UUID
+from uuid import uuid4
+from fastapi import FastAPI
+from typing import List
+import json
+
+f = open('db.json', 'r')
+data = json.load(f)
 
 app = FastAPI()
 
-items = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
-    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
-}
-
-
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
-
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-    tags: set[str] = set()
-    images: list[Image] | None = None
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "name": "Foo",
-                "description": "A very nice Item",
-                "price": 35.4,
-                "tax": 3.2,
-                "tags": ["Foo", "Bar"],
-                "images": [
-                    {
-                        "url": "https://example.com/baz.jpg",
-                        "name": "The Foo live"
-                    }
-                ]
-            }
-        }
-
-
-class Offer(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    items: list[Item]
-
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
-class UserIn(BaseModel):
-    username: str
-    password: str
-    email: EmailStr
-    full_name: str | None = None
-
-
-class UserOut(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: str | None = None
-
-
-@app.post("/user/", response_model=UserOut)
-async def create_user(user: UserIn):
-    return user
-
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-
-@app.post("/items/", response_model=Item)
-async def create_item(item: Item):
-    return item
-
-
-@app.put("/items/{item_id}")
-async def update_item(
-        *,
-        item_id: int,
-        item: Item = Body(
-            examples={
-                "normal": {
-                    "summary": "A normal example",
-                    "description": "A **normal** item works correctly.",
-                    "value": {
-                        "name": "Foo",
-                        "description": "A very nice Item",
-                        "price": 35.4,
-                        "tax": 3.2,
-                    },
-                },
-                "converted": {
-                    "summary": "An example with converted data",
-                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
-                    "value": {
-                        "name": "Bar",
-                        "price": "35.4",
-                    },
-                },
-                "invalid": {
-                    "summary": "Invalid data is rejected with an error",
-                    "value": {
-                        "name": "Baz",
-                        "price": "thirty five point four",
-                    },
-                },
-            },
-        ),
-):
-    results = {"item_id": item_id, "item": item}
-    return results
-
-
-@app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
-async def read_item(item_id: str):
-    return items[item_id]
-
-
-@app.post("/offers/")
-async def create_offer(offer: Offer):
-    return offer
-
-
-@app.post("/images/multiple/")
-async def create_multiple_images(images: list[Image]):
-    return images
-
-
-@app.post("/index-weights/")
-async def create_index_weights(weights: dict[int, float]):
-    return weights
-
 
 @app.get("/")
-async def read_main():
-    return {"msg": "Hello World"}
+# read all data from db.json
+def read_root():
+    return data
+
+# --------------------------------------- Product --------------------------------------- #
+# add new product to db.json
+@app.post("/products/add")
+def add_product(name: str, description: str, price: float, stock: int):
+    last_product_id = data['products'][-1]['id']
+    new_product = {
+        "id": last_product_id + 1,
+        "name": name,
+        "description": description,
+        "price": price,
+        "stock": stock
+    }
+    data['products'].append(new_product)
+    with open('db.json', 'w') as f:
+        json.dump(data, f, indent=4)
+    return data
+
+# update product by product_id
+@app.put("/products/update/{product_id}")
+def update_product(product_id: int, name: str, description: str, price: float, stock: int):
+    for product in data['products']:
+        if product['id'] == product_id:
+            product['name'] = name
+            product['description'] = description
+            product['price'] = price
+            product['stock'] = stock
+    with open('db.json', 'w') as f:
+        json.dump(data, f, indent=4)
+    return product
 
 
-client = TestClient(app)
 
+@app.get("/products/{product_id}")
+# read data from db.json by product_id
+def read_product(product_id: int):
+    for product in data['products']:
+        if product['id'] == product_id:
+            return product
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
