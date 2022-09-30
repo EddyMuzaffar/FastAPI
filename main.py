84e3efa82511a7f3cb1c7ef6fe6cae1c39cfd32d
FlorianCohenJoly <floriancohenjoy@gmail.com>
2022-09-30 11:42:48 +0200

    Post avec la bd

--- main.py
+++ main.py
@@ -1,47 +1,31 @@
+import json
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Lieux(BaseModel):
+    id: int
+    name: str
+    location: str
+
 
 app = FastAPI()
 
-donnees = {
-    'lieux': [
-        'Paris',
-        'Lyon',
-        'Marseille',
-        'Montpellier',
-        'Toulon',
-        'Lilles',
-        'Nantes']
-}
+with open('db.json', 'r') as f:
+    people = json.load(f)
 
-@app.get("/lieux")
+
+@app.get("/lieux/")
 def get_lieux():
-    return {'donnees': donnees}
+    return people
+
+
+@app.post("/lieux/")
+def post_lieu(lieu: Lieux):
+    with open('db.json', mode="w") as f:
+        people['lieux'].append(lieu.dict())
+        f.write(json.dumps(people))
+    return people
 
 
 
-@app.post("/lieux/{lieu}")
-def post_lieu(lieu: str):
-    # si le lieu est déjà présent, nous ne l'ajoutons pas aux données
-    if lieu in donnees['lieux']:
-        # donc retourner simplement la réponse avec un message disant qu'il existe déjà
-        return {'donnees': donnees, 'message': "l'emplacement existe déjà"}
-
-    # par contre s'il n'est pas présent, nous ajoutons le lieu aux données
-    else:
-        donnees['lieux'].append(lieu)
-        # réponse de retour
-        return {'donnees': donnees, 'message': "l'emplacement a été ajouté"}
-
-
-
-@app.delete("/lieux/{lieu}")
-def delete_lieu(lieu: str):
-        # si le lieu est présent, supprimez-le
-        if lieu in donnees['lieux']:
-            donnees['lieux'].remove(lieu)
-            # réponse de retour confirmant la suppression
-            return {'data': donnees, 'message': 'le lieu est supprimé'}
-
-        # s'il n'est pas présent, renvoyez simplement la réponse
-        else:
-            return {'data': donnees, 'message': "le lieu n'existe pas"}
